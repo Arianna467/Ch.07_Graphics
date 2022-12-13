d95e01bf2f64a811808db3c79adfc38a3a804c58
learari <learari@urbandaleschools.com>
2022-12-13 09:56:07 -0600

    Final Commit

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,44 @@
-#Sign your name:________________
+#Sign your name: Arianna Lear
 
 '''
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+import arcade
+arcade.open_window(500,400,"My Drawing")
+arcade.set_background_color(arcade.color.ALMOND)
+
+#Grid
+arcade.start_render()
+for i in range(0,510,20):
+    arcade.draw_line(i,0,i,400,arcade.color.BLACK,1)
+for i in range(0,410,20):
+    arcade.draw_line(0,i,510,i,arcade.color.BLACK,1)
+
+#Purple Rectangle
+arcade.draw_lrtb_rectangle_filled(20,80,380,360,arcade.color.PHLOX)
+
+#Tilted Rectangle
+arcade.draw_rectangle_filled(200,260,20,40,arcade.color.BLUSH,40)
+
+#Pac-Man
+arcade.draw_arc_filled(400,320,120,120,arcade.color.YELLOW,30,330)
+
+#Purple Circle
+arcade.draw_circle_filled(260,200,40,arcade.color.WISTERIA)
+
+#Orange Ellipse
+arcade.draw_ellipse_filled(100,100,120,40,arcade.color.AMBER)
+
+#Text
+arcade.draw_text("I love you. I know.",20,180,arcade.color.BRICK_RED,20)
+
+#Line
+arcade.draw_line(80,20,120,60,arcade.color.BLUE)
+
+#Dot
+arcade.draw_point(460,10,arcade.color.RED,5)
+
+arcade.finish_render()
+arcade.run()
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,3 +10,41 @@ We will have a competition to see who can make this flag in the least lines of c
 The record is 16! You will have to use some loops to achieve this.
 CHALLENGE: Can you make the entire flag parametrically? This means if I change the hoist to 520px the flag will resize accordingly.
 '''
+import arcade
+arcade.open_window(494,260)
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+
+#Strips
+for i in range(10,400,40):
+    arcade.draw_line(0,i,510,i,(191,10,48),20)
+
+#Blue
+arcade.draw_rectangle_filled(0,400,375.44,560,(0,40,104))
+
+#Stars
+for i in range(120,300,30):
+    arcade.draw_text("*",8,i,arcade.color.WHITE)
+for i in range(120, 300, 30):
+    arcade.draw_text("*", 41, i, arcade.color.WHITE)
+for i in range(120, 300, 30):
+    arcade.draw_text("*", 74, i, arcade.color.WHITE)
+for i in range(120, 300, 30):
+    arcade.draw_text("*", 107, i, arcade.color.WHITE)
+for i in range(120, 300, 30):
+    arcade.draw_text("*", 140, i, arcade.color.WHITE)
+for i in range(120, 300, 30):
+    arcade.draw_text("*", 173, i, arcade.color.WHITE)
+for i in range(135, 330, 30):
+    arcade.draw_text("*", 157.5, i, arcade.color.WHITE)
+for i in range(135, 330, 30):
+    arcade.draw_text("*", 23.5, i, arcade.color.WHITE)
+for i in range(135, 330, 30):
+    arcade.draw_text("*", 56.5, i, arcade.color.WHITE)
+for i in range(135, 330, 30):
+    arcade.draw_text("*", 89.5, i, arcade.color.WHITE)
+for i in range(135, 330, 30):
+    arcade.draw_text("*", 122.5, i, arcade.color.WHITE)
+
+arcade.finish_render()
+arcade.run()
